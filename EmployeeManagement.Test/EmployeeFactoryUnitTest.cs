9356ddd8ff91f583f75ee379a4ed8ab3b6f3ff4f
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2023-10-21 12:02:05 +0400

    added first test

---
+++ EmployeeManagement.Test/EmployeeFactoryUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeFactoryUnitTest
+    {
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+        {
+            EmployeeFactory employeeFactory = new EmployeeFactory();
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kapil", "Khubchandani", "KKTECH");
+
+            Assert.Equal(2500, employee.Salary);
+            
+        }
+    }
+}
